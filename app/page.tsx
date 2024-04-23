7c568d092511d37004a835fa08b687717365c8c4
Aditya Ranjan Jha <adityaranjan2014@gmail.com>
2024-04-23 13:26:20 +0530

    Add necessary configuration files and initial code for the project

---
+++ app/page.tsx
@@ -0,0 +1,25 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <head>
+        <title>My Portfolio</title>
+      </head>
+
+      {/* Header */}
+
+      {/* Hero */}
+
+      {/* About */}
+
+      {/* Experience */}
+
+      {/* Skils */}
+
+      {/* Projects */}
+
+      {/* Contact Me */}
+    </div>
+  );
+}
