fb7a24ad47a2c6c2bf1e40c3a16bd301180b3489
Aditya Ranjan Jha <adityaranjan2014@gmail.com>
2024-05-09 01:30:08 +0530

    Refactor ExperienceCard component to use local image file instead of remote URL

--- app/ExperienceCard/ExperienceCard.tsx
+++ app/ExperienceCard/ExperienceCard.tsx
@@ -1,12 +1,15 @@
+'use client'
 import React from 'react'
+import hackto from '../../public/hacktober.jpeg'
+import Image from 'next/image'
 
 type Props = {}
 function ExperienceCard({}: Props) {
   return (
     <article>
         <img
-        className='w-86 h-86 rounded-full xl:h-[200px] xl:w-[200px]'
-            src="https://github.com/AdityaRanjanJha/portfolio_new/blob/master/public/hacktober.jpeg"
+        className='w-32 h-32 rounded-full xl:h-[200px] xl:w-[200px] object-cover object-center'
+            src="/hacktober.jpg"
             alt="image not found !!"
         />
     </article>
